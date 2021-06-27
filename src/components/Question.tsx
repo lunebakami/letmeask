1be25e734dfb706b10828c9638a2ab43fc96aebf
Luis Santos <luis.santos.taiba@gmail.com>
2021-06-27 13:32:24 -0300

    feat: adding fourth and fifth class content

---
+++ src/components/Question.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+import cx from 'classnames';
+
+import '../styles/question.scss';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+  children?: ReactNode;
+};
+
+export function Question({
+  content,
+  author,
+  children,
+  isAnswered = false,
+  isHighlighted = false,
+}: QuestionProps) {
+  return (
+    <div
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
+}
